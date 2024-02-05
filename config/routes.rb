e9df5c7e7ac47a9c29d83ef1d502fa567e00134f
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-02-05 15:00:15 -0600

    Add link to books on index listing and nav bar with logo and link for root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/books', to: 'books#index'
-  get '/books/:id', to: 'books#show'
+  get '/books/:id', to: 'books#show', as: 'book'
+
+  root 'books#index'
 end
