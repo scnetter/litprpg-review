b711bae2649b979430a89469192f78f924c1defb
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-02-06 14:10:03 -0600

    Add route for editing a book and create before_action to find book.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,18 @@
 class BooksController < ApplicationController
+  before_action :find_book, except: [:index, :new, :create]
+  
   def index
     @books = Book.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  private
+  def find_book
     @book = Book.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show', as: 'book'
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
 
   root 'books#index'
 end
