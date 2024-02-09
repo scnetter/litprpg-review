c4cc9517a5990ca5360617aa637e29a5b3a9abab
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-02-09 10:14:29 -0600

    Update error message for rating validation.

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
 
   validates :title, :author, :publish_date, presence: true
   validates :synopsis, length: { minimum: 25 }
-  validates :rating, inclusion: { in: RATINGS }
+  validates :rating, inclusion: { in: RATINGS, message: "must be between 1 and 5" }
   validates :cover_img, format: {
     with: /https:\/\/.+\.(jpg|png|gif)/,
     message: "must be a valid URL"
