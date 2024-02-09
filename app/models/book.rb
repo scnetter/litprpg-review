b4b301f3b6a1bb5df26d9e51fe0df06d09155c2b
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-02-09 10:01:58 -0600

    Add validations to book columns. Issue with rating that needs resolved.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ApplicationRecord
+  RATINGS = %w(1 2 3 4 5)
+
+  validates :title, :author, :publish_date, presence: true
+  validates :synopsis, length: { minimum: 25 }
+  validates :rating, inclusion: { in: RATINGS }
+  validates :cover_img, format: {
+    with: /http:\/\/.+\.(jpg|png|gif)/,
+    message: "must be a valid URL"
+  }
 
   def self.available_now
     where("publish_date < ?", Time.now)
